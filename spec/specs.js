2ad8e1f71b14f01b2d49965e6b21a32f0e88cd25
Chris Sefkin & Jesse James <student@epicodus.com>
2015-05-27 16:50:32 -0700

    stuff

--- spec/specs.js
+++ spec/specs.js
@@ -4,4 +4,10 @@ describe('primeSifter', function() {
     expect(primeSifter(10)).to.eql([2, 3, 5, 7]);
   });
 
+  it("Returns a list of primes from 2 to the given number.", function() {
+    expect(primeSifter(100)).to.eql([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]);
+  });
+
+
+
 });
